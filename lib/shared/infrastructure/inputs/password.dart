9c121589d43afc7d4316d2cd4dd3ebc9880c712a
Diego Esquivel <u202116749@upc.edu.pe>
2025-10-07 16:33:38 -0500

    feat(shared): add shared utilities and widgets

---
+++ lib/shared/infrastructure/inputs/password.dart
@@ -0,0 +1,42 @@
+import 'package:formz/formz.dart';
+
+// Define input validation errors
+enum PasswordError { empty, length, format }
+
+// Extend FormzInput and provide the input type and error type.
+class Password extends FormzInput<String, PasswordError> {
+
+
+  static final RegExp passwordRegExp = RegExp(
+    r'(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$',
+  );
+
+  // Call super.pure to represent an unmodified form input.
+  const Password.pure() : super.pure('');
+
+  // Call super.dirty to represent a modified form input.
+  const Password.dirty( super.value ) : super.dirty();
+
+
+  String? get errorMessage {
+    if ( isValid || isPure ) return null;
+
+    if ( displayError == PasswordError.empty ) return 'The field is required';
+    if ( displayError == PasswordError.length ) return 'It must be at least 6 characters long';
+    if ( displayError == PasswordError.format ) return 'It must have capital letters, letters and a number';
+
+    return null;
+  }
+
+
+  // Override validator to handle validating a given input value.
+  @override
+  PasswordError? validator(String value) {
+
+    if ( value.isEmpty || value.trim().isEmpty ) return PasswordError.empty;
+    if ( value.length < 3 ) return PasswordError.length;
+    // if ( !passwordRegExp.hasMatch(value) ) return PasswordError.format;
+
+    return null;
+  }
+}
---
+++ lib/shared/infrastructure/services/key_value_storage_service_impl.dart
@@ -0,0 +1,53 @@
+import 'package:pet_tracker/shared/infrastructure/services/key_value_storage_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class KeyValueStorageServiceImpl extends KeyValueStorageService {
+  Future<SharedPreferences> getSharedPrefs() async {
+    return await SharedPreferences.getInstance();
+  }
+
+  @override
+  Future<T?> getValue<T>(String key) async {
+    final prefs = await getSharedPrefs();
+    final value = prefs.get(key);
+
+    if (T == int) {
+      return prefs.getInt(key) as T?;
+    } else if (T == double) {
+      return prefs.getDouble(key) as T?;
+    } else if (T == String) {
+      return prefs.getString(key) as T?;
+    } else if (T == bool) {
+      return prefs.getBool(key) as T?;
+    } else if (T == List<String>) {
+      return prefs.getStringList(key) as T?;
+    } else {
+      throw UnimplementedError('Get not implemented for type $T');
+    }
+  }
+
+  @override
+  Future<void> setKeyValue<T>(String key, T value) async {
+    final prefs = await getSharedPrefs();
+
+    if (value is int) {
+      await prefs.setInt(key, value);
+    } else if (value is double) {
+      await prefs.setDouble(key, value);
+    } else if (value is String) {
+      await prefs.setString(key, value);
+    } else if (value is bool) {
+      await prefs.setBool(key, value);
+    } else if (value is List<String>) {
+      await prefs.setStringList(key, value);
+    } else {
+      throw UnimplementedError('Set not implemented for type $T');
+    }
+  }
+
+  @override
+  Future<bool> removeKey(String key) async {
+    final prefs = await getSharedPrefs();
+    return await prefs.remove(key);
+  }
+}
